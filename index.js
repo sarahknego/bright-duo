6caa3ff97fcc942bde009e7d4a8da8ea8204d728
sarahknego <sarah.knego@yahoo.com>
2024-04-05 21:54:25 -0500

    initial commit

---
+++ index.js
@@ -0,0 +1,35 @@
+document.addEventListener("DOMContentLoaded", function() {
+const prevArrow = document.querySelector(".prev");
+const nextArrow = document.querySelector(".next");
+const prevArrowMobile = document.querySelector(".prev-mobile");
+const nextArrowMobile = document.querySelector(".next-mobile");
+const carouselContainer = document.querySelector(".carousel-container");
+
+  prevArrow.addEventListener("click", function() {
+  carouselContainer.scrollBy({
+    left: -carouselContainer.offsetWidth,
+    behavior: "smooth"
+  });
+});
+
+prevArrowMobile.addEventListener("click", function() {
+  carouselContainer.scrollBy({
+    left: -carouselContainer.offsetWidth,
+    behavior: "smooth"
+  });
+});
+
+nextArrow.addEventListener("click", function() {
+  carouselContainer.scrollBy({
+    left: carouselContainer.offsetWidth,
+    behavior: "smooth"
+  });
+});
+
+nextArrowMobile.addEventListener("click", function() {
+  carouselContainer.scrollBy({
+    left: carouselContainer.offsetWidth,
+    behavior: "smooth"
+  });
+});
+});
